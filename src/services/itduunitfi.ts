c5743ac1d852aa640114f70ab14c166649fbed61
Ville Kautiainen <kautiainen.ville@gmail.com>
2024-10-30 12:54:54 +0200

    toggle prod

--- src/services/itduunitfi.ts
+++ src/services/itduunitfi.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const getBatch = async (pageNumber: number) => {
-  //const defaultUrl = `/.netlify/functions/fetchJobs?page=${pageNumber}`
-  const defaultUrl = `/api/itduunit/${pageNumber}`
+  const defaultUrl = `/.netlify/functions/fetchJobs?page=${pageNumber}`
+  //const defaultUrl = `/api/itduunit/${pageNumber}`
   const {data} = await axios.get(defaultUrl)
   return data
 }
